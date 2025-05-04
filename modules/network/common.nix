9a893ec32e68dd9ac7d910ba577874afce54890a
Yunus Emre ALTINER <131378214+yunusemre12500@users.noreply.github.com>
2025-05-04 21:03:35 +0300

    disable IPv6 at system level when networking.enableIPv6 is set to false

--- modules/network/common.nix
+++ modules/network/common.nix
@@ -1,4 +1,4 @@
-{ lib, ... }: {
+{ config, lib, ... }: {
     services.resolved.enable = true;
     networking = {
         dhcpcd.enable = false;
@@ -6,6 +6,11 @@
         firewall = {
             allowPing = false;
             enable = false;
+        };
+        sysctl = lib.mkIf (!config.networking.enableIPv6) {
+            "net.ipv6.conf.all.disable_ipv6" = 1;
+            "net.ipv6.conf.default.disable_ipv6" = 1;
+            "net.ipv6.conf.lo.disable_ipv6" = 1;
         };
         useDHCP = false;
     };
