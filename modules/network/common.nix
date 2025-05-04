201daeab5739b11b5513c798c9ef173f951982d8
Yunus Emre ALTINER <131378214+yunusemre12500@users.noreply.github.com>
2025-05-04 21:12:19 +0300

    hotfix

--- modules/network/common.nix
+++ modules/network/common.nix
@@ -1,5 +1,9 @@
 { config, lib, ... }: {
-    services.resolved.enable = true;
+    boot.sysctl = lib.mkIf (!config.networking.enableIPv6) {
+        "net.ipv6.conf.all.disable_ipv6" = 1;
+        "net.ipv6.conf.default.disable_ipv6" = 1;
+        "net.ipv6.conf.lo.disable_ipv6" = 1;
+    };
     networking = {
         dhcpcd.enable = false;
         enableIPv6 = false;
@@ -7,11 +11,7 @@
             allowPing = false;
             enable = false;
         };
-        sysctl = lib.mkIf (!config.networking.enableIPv6) {
-            "net.ipv6.conf.all.disable_ipv6" = 1;
-            "net.ipv6.conf.default.disable_ipv6" = 1;
-            "net.ipv6.conf.lo.disable_ipv6" = 1;
-        };
         useDHCP = false;
     };
+    services.resolved.enable = true;
 }
