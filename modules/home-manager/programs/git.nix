af53ac358e85e20f0129446e4b88f850cac565d0
yunusemre12500 <131378214+yunusemre12500@users.noreply.github.com>
2025-05-27 17:28:08 +0300

    fix: fix typo

--- modules/home-manager/programs/git.nix
+++ modules/home-manager/programs/git.nix
@@ -4,7 +4,7 @@
     enable = true;
     signing = {
       format = "openpgp";
-      key = "ed25519/0363B60B213CB2BB";
+      key = "0363B60B213CB2BB";
       signByDefault = true;
     };
     userEmail = "131378214+yunusemre12500@users.noreply.github.com";
